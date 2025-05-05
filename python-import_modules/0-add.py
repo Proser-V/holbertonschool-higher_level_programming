2cc7d59060cc0fe45bb827b929a42e5484fc71ff
Proser-V <10530@holbertonstudents.com>
2025-05-05 22:30:26 +0200

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,8 +2,8 @@
 import add_0
 from add_0 import add
 
-a = 1
-b = 2
-result = add(a, b)
-
-print('{} + {} = {}'.format(a, b, result))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    result = add(a, b)
+    print('{} + {} = {}'.format(a, b, result))
