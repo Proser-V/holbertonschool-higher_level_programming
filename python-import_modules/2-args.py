ee0b33772b4730fe89540f0d87cb975bd6b29c2c
Proser-V <10530@hotbertonstudents.com>
2025-05-06 11:10:00 +0200

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 	if len(sys.argv) == 2:
 		print('{} argument:'.format((len(sys.argv) - 1)))
 		i = 1
-		for i, arg in sys.argv[1:]:
+		for arg in sys.argv[1:]:
 			print("{}: {}".format(i, arg))
 	elif len(sys.argv) == 1:
 		print('{} arguments.'.format((len(sys.argv) - 1)))
@@ -15,4 +15,3 @@ if __name__ == "__main__":
 		for arg in sys.argv[1:]:
 			print("{}: {}".format(i, arg))
 			i = i + 1
-			
