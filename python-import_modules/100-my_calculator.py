fe2f6e7a5449ac3b15458acca75da6a4a091b445
Proser-V <10530@hotbertonstudents.com>
2025-05-06 15:17:12 +0200

    100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-from calculator_1 import (add, sub, mul, div)
-import sys
+from calculator_1 import add, sub, mul, div
+from sys import argv, exit
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
+    a = int(argv[1])
+    b = int(argv[3])
     result = 0
 
-    if sys.argv[2] == "+":
+    if argv[2] == "+":
         result = add(a, b)
         print('{} + {} = {}'.format(a, b, result))
-    elif sys.argv[2] == "-":
+    elif argv[2] == "-":
         result = sub(a, b)
         print('{} - {} = {}'.format(a, b, result))
-    elif sys.argv[2] == "*":
+    elif argv[2] == "*":
         result = mul(a, b)
         print('{} * {} = {}'.format(a, b, result))
-    elif sys.argv[2] == "/":
+    elif argv[2] == "/":
         result = div(a, b)
         print('{} / {} = {}'.format(a, b, result))
     else:
