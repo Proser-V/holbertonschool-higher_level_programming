68c3ba04c22da85b2bbf2d347ba61b435ef96ed7
Proser-V <10530@hotbertonstudents.com>
2025-05-06 14:41:33 +0200

    101-remove_char_at.py

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    cpy = '{}{}'.format(str[:n], str[(n + 1):], end="")
-    return cpy
+    if n >= 0:
+        str = '{}{}'.format(str[:n], str[(n + 1):], end="")
+    return str
