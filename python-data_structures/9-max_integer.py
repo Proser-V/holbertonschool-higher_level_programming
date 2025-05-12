de5503018381159c07f33d37c33aafc146befe8f
Proser-V <10530@holbertonstudents.com>
2025-05-12 15:21:00 +0200

    9-max_integer.py

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pyhton3
+
+def max_integer(my_list=[]):
+    if my_list == "":
+        return
+    i = 0
+    max_int = 0
+    while i < len(my_list):
+        if max_int < my_list[i]:
+            max_int = my_list[i]
+        else:
+            pass
+        i += 1
+    return max_int
