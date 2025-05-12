02396fd71152a271d55fad090da034c22a324831
Proser-V <10530@holbertonstudents.com>
2025-05-12 10:01:58 +0200

    Checker wtf?!

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -3,6 +3,5 @@
 def print_list_integer(my_list=[]):
     i = 0
     while i < len(my_list):
-        str = '{}'.format(my_list[i])
-        print(str)
+        print('{}'.format(my_list[i]))
         i += 1
