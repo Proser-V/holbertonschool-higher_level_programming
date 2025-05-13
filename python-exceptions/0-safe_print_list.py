cd4cce68d549fb5fbeca262c3aed456bf5faf683
Proser-V <10530@holbertonstudents.com>
2025-05-13 13:54:43 +0200

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,6 +7,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             i +=1
     except IndexError:
-        pass
+        print()
+        return i
     print()
     return i
