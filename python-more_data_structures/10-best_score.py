44959e9e3d9143953714276be3562fbb3ddab523
Proser-V <10530@holbertonstudents.com>
2025-05-13 11:03:51 +0200

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,11 +2,11 @@
 
 def best_score(a_dictionary):
     first = True
-    if a_dictionary == {} or a_dictionary == None :
+    if a_dictionary == {} or a_dictionary is None:
         return None
     else:
         for key, value in a_dictionary.items():
-            if first == True:
+            if first is True:
                 best_value = value
                 best_key = key
                 first = False
