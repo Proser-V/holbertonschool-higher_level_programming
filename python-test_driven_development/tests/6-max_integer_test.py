43bc3f71b19fcad2001ddd8ac66205c5d2be5f1e
Proser-V <10530@holbertonstudents.com>
2025-05-14 13:27:28 +0200

    6-max_integer_test.py update unittest to be ok with Holberton function, remove some edge cases tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -13,11 +13,11 @@ class TestMaxInteger(unittest.TestCase):
     def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
 
-    def test_list_strings(self):
+    """def test_list_strings(self):
         self.assertRaises(TypeError, max_integer, list=["try", "me", "dude"])
 
     def test_number_float(self):
-        self.assertRaises(TypeError, max_integer, list=[5.68, 16.35, 52.45])
+        self.assertRaises(TypeError, max_integer, list=[5.68, 16.35, 52.45])"""
 
     def test_multi_types(self):
         self.assertRaises(TypeError, max_integer, list=[5.68, "test_me", 2])
