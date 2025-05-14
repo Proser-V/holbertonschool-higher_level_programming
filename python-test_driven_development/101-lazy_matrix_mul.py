ff5cc723b01ad65afcc1666f9da9a23674f11fc2
Proser-V <10530@holbertonstudents.com>
2025-05-14 17:41:53 +0200

    101-lazy_matrix_mul.py modification

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -10,4 +10,6 @@ def lazy_matrix_mul(m_a, m_b):
     """
     Multiplies two matrices using numpy's matmul function.
     """
+    if not (np.issubdtype(np.asarray(m_a).dtype, np.number) and np.issubdtype(np.asarray(m_b).dtype, np.number)):
+        raise TypeError("Scalar operands are not allowed, use '*' instead")
     return np.matmul(m_a, m_b)
