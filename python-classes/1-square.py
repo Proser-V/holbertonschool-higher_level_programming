05daea5e6cc57289634ed3cb771ecf5f7d8a7a68
Proser-V <10530@hotbertonstudents.com>
2025-05-19 10:20:48 +0200

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module Name: 1-square.py
+
+Description:
+    This module include an empty class that defines a square.
+"""
+
+
+class Square:
+    """
+    An empty class that defines a square
+    """
+    def __init__(self, __size):
+        self.__size = __size
