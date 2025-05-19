8fea5ccde57b11e04913d5b40af7ea443bf2d3b6
Proser-V <10530@hotbertonstudents.com>
2025-05-19 15:18:04 +0200

    0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module Name: 0-rectangle.py
+
+Description:
+    This module include an empty class that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    An empty class that defines a rectangle.
+    """
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+Going further with classes in pyhton
