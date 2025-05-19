8877fa7b66aee79d1c0e6d58f694fdafb00f4774
Proser-V <10530@hotbertonstudents.com>
2025-05-19 10:26:49 +0200

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module Name: 1-square.py
+
+Description:
+    This module include an empty class that defines a square.
+"""
+
+
+class Square:
+    """
+    An empty class that defines a square
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
