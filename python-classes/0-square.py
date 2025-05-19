4101c25a02732a8ae6a3346f05928c901372959e
Proser-V <10530@hotbertonstudents.com>
2025-05-19 10:11:34 +0200

    0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module Name: 0-square.py
+
+Description:
+    This module include an empty class that defines a square.
+"""
+
+
+class Square:
+    """
+    An empty class that defines a square
+    """
+    pass
