2eda36926101eb99bc29609d48b07a462ceb4ceb
Proser-V <10530@holbertonstudents.com>
2025-05-26 00:50:44 +0200

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,11 +3,15 @@
 Module Name: 1-my_list.py
 
 Description:
-    This module include a class "MyList" that inherits from "list".
+    This module include a class that prints a sorted list.
 """
 
 
 class MyList(list):
+#!/usr/bin/python3
+    """
+        This class prints a sorted list.
+    """
     def print_sorted(self):
         my_list = sorted(self)
         print(my_list)
