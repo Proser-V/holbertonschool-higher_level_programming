67936da8bdd6cae734000ca5dfbbca62171713ee
Proser-V <10530@hotbertonstudents.com>
2025-05-26 09:29:53 +0200

    2-is_same_class.py boolean check

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -13,5 +13,6 @@ def is_same_class(obj, a_class):
     A function that checks if an object is exactly an instance of the
     specified class.
     """
+    if isinstance(obj, bool) and a_class == int:
+        return False
     return isinstance(obj, a_class)
-
