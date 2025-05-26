392878cbe80b72336a3f9e8541ef0849df600a23
Proser-V <10530@hotbertonstudents.com>
2025-05-26 10:26:13 +0200

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module Name: 6-base_geometry.py
+
+Description:
+    This module include a class that raise an exception because
+    its method is not implemented?!
+"""
+
+
+class BaseGeometry:
+    """
+        A class that raise an exception because
+        its method is not implemented?!
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
