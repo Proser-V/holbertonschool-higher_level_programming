b24b17de62e91f9ba2972f540d883633cc737d08
Proser-V <10530@hotbertonstudents.com>
2025-05-26 10:06:49 +0200

    2-is_same_class.py isinstance is not ok

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -13,6 +13,4 @@ def is_same_class(obj, a_class):
     A function that checks if an object is exactly an instance of the
     specified class.
     """
-    if isinstance(obj, bool) and a_class == int:
-        return False
-    return isinstance(obj, a_class)
+    return type(obj) is a_class
