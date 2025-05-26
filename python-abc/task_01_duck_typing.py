06dbefb2b643dbd8337c85870c5d18d055ffcafc
Proser-V <10530@hotbertonstudents.com>
2025-05-26 12:42:10 +0200

    task_01_duck_typing.py

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+from abc import ABC, abstractmethod
+
+
+"""
+Module Name: task_01_duck_typing.py
+
+Description:
+    This module include an abstract class and subclasses
+"""
+
+
+class Shape(ABC):
+    """
+        An abstract class with abstract method for sub classes
+    """
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    """
+        A subclass for a typical object
+    """
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.141592653589793 * (self.radius ** 2)
+
+    def perimeter(self):
+        return 2 * 3.141592653589793 * self.radius
+
+
+class Rectangle(Shape):
+    """
+        A subclass for a typical object
+    """
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+def shape_info(Shape):
+    print("Area: {}".format(Shape.area()))
+    print("Perimeter: {}".format(Shape.perimeter()))
