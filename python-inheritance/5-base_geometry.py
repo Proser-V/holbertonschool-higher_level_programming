0b45c8525f746a3ea1953973c5206f9f638a351a
Proser-V <10530@hotbertonstudents.com>
2025-05-26 10:23:39 +0200

    5-base_geometry.py

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module Name: 5-base_geometry.py
+
+Description:
+    This module include an empty class.
+"""
+
+
+class BaseGeometry:
+    """
+        Empty class.
+    """
+    pass
