8882946fc97156912d57938773693ffce3a76fe5
Proser-V <10530@hotbertonstudents.com>
2025-06-02 14:00:47 +0200

    8-class_to_json.py 2 blank lines...

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -6,6 +6,7 @@ description with simple data structure (list, dictionary, string,
 integer and boolean) for JSON serialization of an object.
 """
 
+
 def class_to_json(obj):
     """
     A function that returns the dictionary description with simple data
