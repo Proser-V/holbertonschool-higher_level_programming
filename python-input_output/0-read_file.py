1d5e67202859e1897cfca24a04a9e91ec9d14b6b
Proser-V <10530@hotbertonstudents.com>
2025-06-02 10:48:16 +0200

    0-read_file.py pycodestyle tabs

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,9 +6,9 @@ Description : This module contain a function that reads a file
 
 
 def read_file(filename=""):
-	"""
-	This function reads a file
-	"""
-	with open(filename, "r", encoding="utf-8") as file:
-		content = file.read()
-		print(content, end="")
+    """
+    This function reads a file
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        content = file.read()
+        print(content, end="")
