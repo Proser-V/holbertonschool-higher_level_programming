88b8d84fe20d5bf85d558e87b6ad576d29056bc9
Proser-V <10530@hotbertonstudents.com>
2025-06-02 14:48:49 +0200

    10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Description : This module contain a class that define a student.
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            my_dict = {}
+            for attr in sorted(attrs):
+                if hasattr(self, attr):
+                    my_dict[attr] = getattr(self, attr)
+            return my_dict
+        else:
+            return self.__dict__
--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 """
-Description : This module contain a script that returns the dictionary
-description with simple data structure (list, dictionary, string,
-integer and boolean) for JSON serialization of an object.
+Description : This module contain a class that define a student.
 """
 
 
