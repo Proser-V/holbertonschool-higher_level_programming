53b474bb53e998fb7181db7f681b2caa8b6a1a06
Proser-V <10530@holbertonstudents.com>
2025-06-09 19:47:59 +0200

    task_03_http_server.py another error 404 message

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -32,12 +32,12 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 def run(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=8000):
     server_address = ("", port)
     httpd = server_class(server_address, handler_class)
-    print(f"Starting HTTP server on port {port}...")
+    print(f"Starting HTTP server on port {port}")
     httpd.serve_forever()
 
 if __name__ == "__main__":
