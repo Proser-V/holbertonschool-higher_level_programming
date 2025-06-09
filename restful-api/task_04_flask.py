eeb8e8ead8ff12472dfa85f1b77331bef1bbea92
Proser-V <10530@holbertonstudents.com>
2025-06-09 22:29:05 +0200

    task_04_flask.py what about now?!

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
 from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
+
+
 users = {}
 
 
@@ -13,33 +14,32 @@ def home():
 
 
 @app.route("/data")
-def json_data():
+def get_data():
     return jsonify(list(users.keys()))
 
 
 @app.route("/status")
-def get_status():
+def status():
     return "OK"
 
 
 @app.route("/users/<username>")
 def get_user(username):
-    if username in users.keys():
-        return jsonify(users.get(username))
+    user = users.get(username)
+    if user:
+        return jsonify(user)
     else:
         return jsonify({"error": "User not found"}), 404
 
 
-@app.route("/add_user", methods=['POST'])
+@app.route("/add_user", methods=["POST"])
 def add_user():
-    data = request.get_json()
-    username = data.get("username")
+    user_data = request.get_json()
+    username = user_data.get("username")
     if not username:
         return jsonify({"error": "Username is required"}), 400
-
-    users[username] = data
-
-    return jsonify({"message": "User added", "user": data}), 201
+    users[username] = user_data
+    return jsonify({"message": "User added", "user": user_data}), 201
 
 
 if __name__ == "__main__":
