8d9f2bef848589447083d9111dfe43c3eedf5256
Proser-V <10530@hotbertonstudents.com>
2025-06-11 17:22:44 +0200

    API tests

---
+++ alone_test/APItest.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort
+
+app = Flask(__name__)
+api = Api(app)
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
+video_put_args.add_argument("views", type=int, help="Number of views", required=True)
+video_put_args.add_argument("likes", type=int, help="Number of likes", required=True)
+
+videos = {}
+
+def abort_if_not_ID(video_id):
+    if video_id not in videos:
+        abort(404, message="Could not find video...")
+
+def abort_if_ID(video_id):
+    if video_id in videos:
+        abort(409, message="Video already exists with that ID...")
+
+class Video(Resource):
+    def get(self, video_id):
+        abort_if_not_ID(video_id)
+        return videos[video_id]
+
+    def put(self, video_id):
+        abort_if_ID(video_id)
+        args = video_put_args.parse_args()
+        print(args)
+        videos[video_id] = args
+        return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_not_ID(video_id)
+        del videos[video_id]
+        return "", 204
+
+api.add_resource(Video, "/video/<int:video_id>")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
