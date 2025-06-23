1c90579ab76af3f20f8ea83deee79c7bb06494ed
Proser-V <10530@holbertonstudents.com>
2025-06-23 10:20:38 +0200

    2-remove_database.sql

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- A script that creates a table called first_table in the current database in your MySQL server.
+
+CREATE TABLE first_table(
+    id INT PRIMARY AUTO_INCREMENT,
+    name VARCHAR(256)
+)
