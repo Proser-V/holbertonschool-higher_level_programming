40fc1c2d0ebecf7cbe4dcf5640beea8f83cb1f93
Proser-V <10530@holbertonstudents.com>
2025-06-23 09:51:50 +0200

    0-list_databases.sql

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,4 +1,3 @@
-# Script pour lister toutes les bases de données d'un serveur MySQL
-# Utilise la commande mysql avec un utilisateur et un mot de passe
+# !/bin/bash
 
 mysql -u root -p -e "SHOW DATABASES;"
