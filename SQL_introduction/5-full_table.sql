16fe78b2c0db1cf9394b4904db406e2fd3ac1d34
Proser-V <10530@holbertonstudents.com>
2025-06-23 11:23:40 +0200

    5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- A script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
