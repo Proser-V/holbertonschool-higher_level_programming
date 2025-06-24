f1f974fc5cfe11106bac0dd7d99cf8dc30858186
Proser-V <10530@holbertonstudents.com>
2025-06-24 10:29:58 +0200

    1-filter_states.py pycodestyle @_@

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''
-Module that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
+Module that lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa.
 '''
 
 import MySQLdb
