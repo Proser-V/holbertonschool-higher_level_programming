93dd1db47f797b65fa4dcb7785942426d0cd03f7
Proser-V <10530@holbertonstudents.com>
2025-06-24 21:39:27 +0200

    Here we go again for the mad checker

---
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''
+Module that that lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa.
+'''
+
+
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}'
+        )
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).join(City).order_by(State.id, City.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"    {city.id}: {city.name}")
+
+    session.close()
