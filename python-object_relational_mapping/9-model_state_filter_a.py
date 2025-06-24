476c533dc9d13b21b67c0a54565dae8d45ad156a
Proser-V <10530@holbertonstudents.com>
2025-06-24 19:50:02 +0200

    Another looping

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+Module that lists all State objects that contain the letter a from the
+database hbtn_0e_6_usa.
+'''
+
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}'
+        )
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        for letter in state.name:
+            if letter == 'a':
+                print(f"{state.id}: {state.name}")
+                break
+
+    session.close()
