b913f7d0ea6f008e6efefaeb8b7f9db5be7e2bbc
Proser-V <10530@holbertonstudents.com>
2025-06-25 10:00:39 +0200

    100-move_to_utf8.sql

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -2,3 +2,9 @@
 
 DROP DATABASE IF EXISTS btn_0c_0;
 CREATE DATABASE btn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+DROP TABLE IF EXISTS first_table;
+CREATE TABLE first_table(
+    id INT DEFAULT NULL,
+    name VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    score INT DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
