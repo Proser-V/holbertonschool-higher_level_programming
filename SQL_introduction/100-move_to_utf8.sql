b2b0d138a7d7b9af66330c45481ab3fb3bdce12e
Proser-V <10530@holbertonstudents.com>
2025-06-25 09:43:43 +0200

    100-move_to_utf8.sql

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- A script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server.
+
+DROP DATABASE IF EXISTS btn_0c_0;
+CREATE DATABASE btn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
