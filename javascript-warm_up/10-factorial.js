cccee2383ec4490f3aefe6c2ce7ca6ba90a93710
Proser-V <10530@hotbertonstudents.com>
2025-07-08 17:50:06 +0200

    2-arguments.js

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,4 @@
+function factorielle(n) {
+  if (n === 0 || n === 1) return 1;
+  else return n * factorielle(n - 1);
+}
---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+if (argv[2] == null) {
+	console.log("No argument");
+} else if (argv[2] != null && argv[3] == null) {
+	console.log("Argument found")
+} else {
+	console.log("Arguments found")
+}
