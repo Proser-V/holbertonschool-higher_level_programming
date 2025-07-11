494c3d9270aec60a6bf16707f591b1823d595ae1
Proser-V <10530@holbertonstudents.com>
2025-07-11 17:05:36 +0200

    9-add.js

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function add () {
+  const a = process.argv[2];
+  const numA = Number(a);
+  const b = process.argv[3];
+  const numB = Number(b);
+
+  if (Number.isInteger(numA) && Number.isInteger(numB)) {
+    const sum = numA + numB;
+    console.log(sum);
+  } else {
+    console.log('Please pass 2 integers');
+  }
+}
+
+add();
