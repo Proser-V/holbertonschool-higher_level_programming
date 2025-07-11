3322370f9820a79425d464cfcb48f92f2e6d90ca
Proser-V <10530@holbertonstudents.com>
2025-07-11 20:53:06 +0200

    11-second_biggest.js

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+let i = 0;
+const argsNumber = args.length;
+let currentNumber;
+let biggest = Number.MIN_SAFE_INTEGER;
+let secondBiggest = Number.MIN_SAFE_INTEGER;
+
+if (argsNumber === 0 || argsNumber === 1) {
+  secondBiggest = 0;
+} else {
+  biggest = Math.max(...args);
+  currentNumber = args.indexOf(biggest);
+  args.splice(currentNumber, 1)
+  secondBiggest = Math.max(...args);
+}
+console.log(secondBiggest);
