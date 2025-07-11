b6ec16df9650be050bfe9d941551c0d09da431d0
Proser-V <10530@holbertonstudents.com>
2025-07-11 17:29:45 +0200

    10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,4 +1,12 @@
-function factorielle (n) {
-  if (n === 0 || n === 1) return 1;
-  else return n * factorielle(n - 1);
+#!/usr/bin/node
+
+function factorielle (a) {
+  if (a == undefined) return 1;
+  const numA = Number(a);
+  if (a == 0 || a == 1) return 1;
+  if (Number.isInteger(numA)){
+      return a * factorielle(numA - 1);
+  }
 }
+
+console.log(factorielle(process.argv[2]));
