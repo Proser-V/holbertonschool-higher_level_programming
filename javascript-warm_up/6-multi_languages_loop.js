ff5d5977daebd3839bbc2c2ad276f00a0d4107a5
Proser-V <10530@holbertonstudents.com>
2025-07-11 16:19:57 +0200

    6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const list_to_print = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const listToPrint = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for(let i = 0; i < 3; i++) {
-    console.log(list_to_print[i])
+for (let i = 0; i < 3; i++) {
+  console.log(listToPrint[i]);
 }
